ff1fa246d2dcef5d58bbda4a29a9d3506e2b4e52
BoiSa <boikoff.aleksandr2016@yandex.ru>
2023-05-24 00:35:55 +0300

    Add project files.

---
+++ Abstract/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var objAbstract = typeof(RuntimeTypeHandle).GetMethod("Allocate", BindingFlags.Static | BindingFlags.NonPublic)
+                .Invoke(null, new object[] { typeof(Abstract) });
+            Console.WriteLine(objAbstract.GetType());
+            Console.ReadLine();
+        }
+    }
+
+    public abstract class Abstract
+    {
+    }
+
+}
